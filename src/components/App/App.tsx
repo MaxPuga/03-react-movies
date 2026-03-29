2fa0541f396189dca37f2c7c119aebd4ad10ea14
Max <maxpuga87@gmail.com>
2026-03-29 13:03:25 +0200

    update all

---
+++ src/components/App/App.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { Toaster, toast } from "react-hot-toast";
+
+import type { Movie } from "../../types/movie";
+import { fetchMovies } from "../../services/movieService";
+
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
+
+import css from "./App.module.css";
+
+export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const handleSearch = async (query: string) => {
+    try {
+      setMovies([]);
+      setError(false);
+      setLoading(true);
+
+      const data = await fetchMovies(query);
+
+      if (data.length === 0) {
+        toast.error("No movies found for your request.");
+      }
+
+      setMovies(data);
+    } catch {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className={css.app}>
+      <Toaster />
+
+      <SearchBar onSubmit={handleSearch} />
+
+      {loading && <Loader />}
+      {error && <ErrorMessage />}
+      {movies.length > 0 && (
+        <MovieGrid movies={movies} onSelect={setSelectedMovie} />
+      )}
+
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => setSelectedMovie(null)}
+        />
+      )}
+    </div>
+  );
+}
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import css from "./SearchBar.module.css";
+
+interface SearchBarProps {
+  onSubmit: (query: string) => void;
+}
+
+export default function SearchBar({ onSubmit }: SearchBarProps) {
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (formData: FormData) => {
+    const value = formData.get("query") as string;
+
+    if (!value.trim()) {
+      toast.error("Please enter your search query.");
+      return;
+    }
+
+    onSubmit(value);
+
+    setQuery("");
+  };
+
+  return (
+    <header className={css.header}>
+      <div className={css.container}>
+        <a
+          href="https://www.themoviedb.org/"
+          target="_blank"
+          rel="noreferrer"
+          className={css.link}
+        >
+          Powered by TMDB
+        </a>
+
+        <form
+          className={css.form}
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit(new FormData(e.currentTarget));
+          }}
+        >
+          <input
+            className={css.input}
+            name="query"
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Search movies..."
+          />
+          <button className={css.button} type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </header>
+  );
+}
